6583d36e6f77327e9936377e94470c90a27d463d
minnlee <87851602+minnlee@users.noreply.github.com>
2021-07-26 12:00:06 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 const CONFIG = {
-    introTitle: 'Babe à!',
+    introTitle: 'Là lđm thì ấn vào đây!',
     introDesc: `Trái đất vốn lạ thường
     Mà sao em cứ đi nhầm đường
     Lạc vào tim anh lẻ loi
