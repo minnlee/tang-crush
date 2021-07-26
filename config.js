0cfe2407e217115bc0863137ea25eac039c2e59b
minnlee <87851602+minnlee@users.noreply.github.com>
2021-07-26 14:44:20 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,19 +1,19 @@
 const CONFIG = {
     introTitle: 'Là lđm thì ấn vào đây!',
-    introDesc:`Trái đất vốn lạ thường
+    introDesc: `Trái đất vốn lạ thường
     Mà sao em cứ đi nhầm đường
     Lạc vào tim anh lẻ loi
-    Đằng sau chữ yêu đây là thương,
+    Đằng sau chữ yêu đây là thương`,
     btnIntro: 'hiiiii',
-    title: 'Tôi từ lâu đã thích anh rồi 😙',
-    desc: 'Chỉ mong hai ta thành đôi ',
-    btnYes: 'Đã thích bạn rất lâu rồi <33',
+    title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 😙',
+    desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
+    btnYes: 'Mình từ lâu đã thích bạn rồi <33',
     btnNo: 'Không, chúng mình chỉ là friendzone :3',
     question:'Trên thế giới hơn 7 tỉ người mà sao bạn lại thích mình <3',
     btnReply: 'Gửi cho lnm <3',
     reply: 'Vì đáng yêu quá đấy <33333333',
     mess: 'Mình biết mà 🥰. Yêu bạn nhiều nhiều 😘😘',
-    messDesc: 'Tối nay 12h solo nhé .',
+    messDesc: 'Tối nay 12h solo nhé.',
     btnAccept: 'Okiiiii tới lun <3',
     messLink: 'http://fb.com' //link mess của các bạn. VD: https://m.me/nam.nodemy
 }
